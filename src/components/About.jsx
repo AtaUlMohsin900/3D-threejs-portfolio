24fba27238a75ea387a561a107680bc46cfcb7b8
Ata Ul Mohsin Khalid <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-05-06 14:59:56 +0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -51,28 +51,8 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[20px] max-w-3xl leading-[30px]"
       >
-        Hi , there
-        i, am AFAR IQBAL (FCCA, M.COM) Chief Financial Officer(CFO) Mr. Zafar Iqbal is
-        a Qualified Fellow Chartered Certified Accountant (UK) with 15 years of
-        diversified experience in accounts, finance, and administration. A
-        professional accountant with experience in wide variety of industries
-        like Telecommunication, Education, Retail and Quick Service Restaurants.
-        Goal-oriented individual, with strong leadership trait and with a
-        demonstrated track record of Oracle based GL Accounting & Analysis of
-        financial reports, process & system development. He is a competent CFO
-        beyond the traditional responsibilities of financial management. He is a
-        skilled communicator, able to effectively translate financial
-        information into actionable insights for both executive teams and
-        stakeholders. Zafar possess excellent interpersonal skills, fostering
-        collaboration across departments and building strong relationships with
-        investors, auditors, and banking institutions. Mr Zafar adept at
-        attracting and retaining top financial talent, nurturing professional
-        development, and fostering a cohesive and high-performing finance
-        department. He is a reliable and proactive team member who takes
-        ownership of his work and consistently exceeds expectations. With his
-        commitment to excellence and a drive for continuous learning, He
-        embodies the qualities of a competent professional who is instrumental
-        in the success of our team and the satisfaction of our clients.
+       Hi there, I am Ataul Mohsin Khalid, a Software Developer with 2 years of diversified experience in software and web development. My journey into the realm of programming began with a deep-seated passion for creating innovative solutions. Throughout my career, I have honed my skills across various domains, showcasing proficiency in software development tools, web development, AI, backend development, and DevOps tools such as Jenkins, Ansible, Docker, Kubernetes, and beyond. My expertise in software development tools spans a wide spectrum, where I adeptly navigate through the intricacies of languages and frameworks to craft robust and scalable applications. Whether it's harnessing the power of Python for its versatility or leveraging the efficiency of Java for enterprise-grade solutions, I thrive on the challenge of utilizing the right tools for the task at hand. In the realm of web development, I have spearheaded projects that seamlessly blend creativity with functionality, delivering immersive user experiences across diverse platforms. My proficiency extends to both frontend and backend development, where I employ cutting-edge technologies like React, Angular, Node.js, and Django to bring dynamic web applications to life. Moreover, my foray into AI has been marked by a fervent exploration of Generative AI, where I delve into the realms of neural networks and machine learning algorithms to create intelligent systems that push the boundaries of innovation. Cloud computing stands as a cornerstone of my expertise, where I harness the scalability and flexibility of cloud platforms to architect robust and resilient solutions that meet the evolving needs of modern enterprises. Furthermore, my proficiency in DevOps tools underscores my commitment to streamlining the software development lifecycle, where I orchestrate seamless deployment pipelines and automate processes for enhanced efficiency and reliability. With a keen eye for detail and a penchant for problem-solving, I thrive in dynamic environments where innovation flourishes, driving forward-thinking initiatives that redefine the landscape of technology. My journey as a Software Developer is defined by a relentless pursuit of excellence, where each project serves as an opportunity to push the boundaries of what's possible and leave a lasting impact on the world of technology.
+       
       </motion.p>
       <div className=" mt-20 mb-20 flex flex-wrap  gap-10">
         {services.map((service, index) => {
